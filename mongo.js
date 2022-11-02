6cfe6a11f1ef1f743abe2aea746c5434d56881f3
Suspens69 <hr@lemelle.onmicrosoft.com>
2022-11-02 21:45:54 +0100

    recriture

--- mongo.js
+++ mongo.js
@@ -1,16 +1,20 @@
-import mongoose from 'mongoose'
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+import mongoose from "mongoose";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
 mongoose.Promise = global.Promise;
 
 // Connect MongoDB at default port 27017.
-mongoose.connect('mongodb://192.168.0.40:27017/test', {
+mongoose.connect(
+  `mongodb://${process.env.MONGO_URL}:${process.env.MONGO_PORT}/test`,
+  {
     useNewUrlParser: true,
-}, (err) => {
+  },
+  (err) => {
     if (!err) {
-        console.log('MongoDB Connection Succeeded.')
+      console.log("MongoDB Connection Succeeded.");
     } else {
-        console.log('Error in DB connection: ' + err)
+      console.log("Error in DB connection: " + err);
     }
-});
+  }
+);
