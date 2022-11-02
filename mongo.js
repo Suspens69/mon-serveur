ee7d6f23a4db6b39f1022dd80607e57151fe9f64
suspens69 <hr@lemelle.onmicrosoft.com>
2022-11-02 21:09:59 +0100

    maj de mongo

--- mongo.js
+++ mongo.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 mongoose.Promise = global.Promise;
 
 // Connect MongoDB at default port 27017.
-mongoose.connect('mongodb://localhost:27017/test', {
+mongoose.connect('mongodb://192.168.0.40:27017/test', {
     useNewUrlParser: true,
 }, (err) => {
     if (!err) {
