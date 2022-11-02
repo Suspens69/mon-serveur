0023853d114d87c98edc34414a3120cfe8731347
Suspens69 <hr@lemelle.onmicrosoft.com>
2022-11-02 21:33:41 +0100

    test doenv

--- mongo.js
+++ mongo.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose'
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
 mongoose.Promise = global.Promise;
 
